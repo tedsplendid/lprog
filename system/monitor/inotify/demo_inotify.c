7dd43910929390c30182a733ed64e980603f87ac
tedsplendid <tedsplendid@gmail.com>
2017-11-17 13:06:15 +0800

    lprog: inotify: add inotify demo
    Usage:
            ./inotify {files or folders to monitor}
    Example:
            1. ./inotify . (to monitor current folder
            2. ./inotify file_name (to monitor the specified file
    
    Change-Id: I68cb5ae5a7a5eeeb8321818e44502ce6da48772a

---
+++ system/monitor/inotify/demo_inotify.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/inotify.h>
+#include <limits.h>
+
+#define ERREXIT(msg...) {    \
+	fprintf(stderr, msg); \
+	_exit(EXIT_FAILURE);  \
+}
+
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+
+void usage(char* argv[]) {
+	printf("Usage:\n");
+	printf("\t%s {files or folders to monitor}\n", argv[0]);
+	printf("Example:\n");
+	printf("\t1. %s . (to monitor current folder\n", argv[0]);
+	printf("\t2. %s file_name (to monitor the specified file\n", argv[0]);
+	printf("\n");
+}
+
+/* Display information from inotify_event structure */
+void displayInotifyEvent(struct inotify_event *i) {
+    printf("    wd =%2d; ", i->wd);
+    if (i->cookie > 0)
+        printf("cookie =%4d; ", i->cookie);
+    printf("mask = ");
+    if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
+    if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+    if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
+    if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
+    if (i->mask & IN_CREATE)        printf("IN_CREATE ");
+    if (i->mask & IN_DELETE)        printf("IN_DELETE ");
+    if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
+    if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
+    if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
+    if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
+    if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
+    if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
+    if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
+    if (i->mask & IN_OPEN)          printf("IN_OPEN ");
+    if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
+    if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+    printf("\n");
+
+    if (i->len > 0)
+        printf("        name = %s\n", i->name);
+}
+
+int main(int argc, char *argv[]) {
+    int inotifyFd, wd, j;
+    char buf[BUF_LEN] __attribute__ ((aligned(8)));
+    ssize_t numRead;
+    char *p;
+    struct inotify_event *event;
+
+    if (argc < 2 || strcmp(argv[1], "--help") == 0) {
+		usage(argv);
+		_exit(EXIT_SUCCESS);
+	}
+
+    inotifyFd = inotify_init();                 /* Create inotify instance */
+    if (inotifyFd == -1)
+        ERREXIT("can't create inotify fd\n");
+
+	/* how much data we want to monitor */
+    for (j = 1; j < argc; j++) {
+        wd = inotify_add_watch(inotifyFd, argv[j], IN_ALL_EVENTS);
+        if (wd == -1)
+            ERREXIT("inotify_add_watch");
+
+        printf("Watching %s using wd %d\n", argv[j], wd);
+    }
+
+	/* Read events forever */
+    for (;;) {
+        numRead = read(inotifyFd, buf, BUF_LEN);
+        if (numRead == 0)
+            fprintf(stderr, "read() from inotify fd returned 0!");
+
+        if (numRead == -1)
+            ERREXIT("read failed\n");
+
+        printf("Read %ld bytes from inotify fd\n", (long) numRead);
+
+        /* Process all of the events in buffer returned by read() */
+        for (p = buf; p < buf + numRead; ) {
+            event = (struct inotify_event *) p;
+            displayInotifyEvent(event);
+
+            p += sizeof(struct inotify_event) + event->len;
+        }
+    }
+
+    _exit(EXIT_SUCCESS);
+}
